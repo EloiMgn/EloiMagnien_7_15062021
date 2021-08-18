5a72e0367f8e96d1652287a26fd81af3a3d4adaf
Eloi Magnien <eloi.magnien@gmail.com>
2021-08-18 11:31:35 +0200

    V2 forEach => for

--- JS/filteringRecipes.js
+++ JS/filteringRecipes.js
@@ -6,41 +6,41 @@ import { STATE } from './state.js';
 export function onClicFilterRecipes (option) {
   // chercher les recette qui ont de chipList[i] dans les recettes en display true
   if (option.classList.contains('option__ingredients')) {
-    STATE.forEach(recipe => {
-      if (recipe.display === true) {
-        if (!recipe.ingredients.map(e => e.ingredient).includes(option.innerHTML)) {
-          recipe.display = false;
+    for (let i = 0; i < STATE.length; i++) {
+      if (STATE[i].display === true) {
+        if (!STATE[i].ingredients.map(e => e.ingredient).includes(option.innerHTML)) {
+          STATE[i].display = false;
         }
       }
-    });
+    }
   } else if (option.classList.contains('option__appliances')) {
-    STATE.forEach(recipe => {
-      if (recipe.display === true) {
-        if (!recipe.appliance.includes(option.innerHTML)) {
-          recipe.display = false;
+    for (let i = 0; i < STATE.length; i++) {
+      if (STATE[i].display === true) {
+        if (!STATE[i].appliance.includes(option.innerHTML)) {
+          STATE[i].display = false;
         }
       }
-    });
+    }
   } else if (option.classList.contains('option__ustensils')) {
-    STATE.forEach(recipe => {
-      if (recipe.display === true) {
-        for (let i = 0; i < recipe.ustensilsList.ustensils.length; i++) {
-          if (recipe.ustensilsList.ustensils[i].includes(option.innerHTML.toLowerCase())) {
-            recipe.ustensilsList.found = true;
+    for (let i = 0; i < STATE.length; i++) {
+      if (STATE[i].display === true) {
+        for (let j = 0; j < STATE[i].ustensilsList.ustensils.length; j++) {
+          if (STATE[i].ustensilsList.ustensils[j].includes(option.innerHTML.toLowerCase())) {
+            STATE[i].ustensilsList.found = true;
             break;
-          } else if (!recipe.ustensilsList.ustensils[i].includes(option.innerHTML.toLowerCase())) {
-            recipe.ustensilsList.found = false;
+          } else if (!STATE[i].ustensilsList.ustensils[j].includes(option.innerHTML.toLowerCase())) {
+            STATE[i].ustensilsList.found = false;
           }
         }
-        if (recipe.ustensilsList.found === false) {
-          recipe.display = false;
+        if (STATE[i].ustensilsList.found === false) {
+          STATE[i].display = false;
         }
       }
-    });
+    }
   }
   displayFilterRecipes(STATE);
   displayErrorMessage();
-};
+}
 
 function getUstensilPosition (recipe, option) {
   for (let i = 0; i < recipe.ustensilsList.ustensils.length; i++) {
@@ -61,22 +61,22 @@ function getUstensilPosition (recipe, option) {
 
 export function onCloseFilterRecipes () {
   if (selectedElements.length > 0) {
-    selectedElements.forEach(chip => {
-      STATE.forEach(recipe => {
-        if (recipe.display === false) {
-          const includesIngredient = recipe.ingredients.map(e => e.ingredient).includes(chip.querySelector('p').innerHTML);
-          const includesAppliance = recipe.appliance.includes(chip.querySelector('p').innerHTML);
-          const includesUstensil = getUstensilPosition(recipe, chip.querySelector('p'));
+    for (let j = 0; j < selectedElements.length; j++) {
+      for (let i = 0; i < STATE.length; i++) {
+        if (STATE[i].display === false) {
+          const includesIngredient = STATE[i].ingredients.map(e => e.ingredient).includes(selectedElements[j].querySelector('p').innerHTML);
+          const includesAppliance = STATE[i].appliance.includes(selectedElements[j].querySelector('p').innerHTML);
+          const includesUstensil = getUstensilPosition(STATE[i], selectedElements[j].querySelector('p'));
           if (includesIngredient || includesAppliance || includesUstensil) {
-            recipe.display = true;
+            STATE[i].display = true;
           }
         }
-      });
-    });
+      }
+    }
   } else if (selectedElements.length === 0) {
-    STATE.forEach(recipe => {
-      recipe.display = true;
-    });
+    for (let i = 0; i < STATE.length; i++) {
+      STATE[i].display = true;
+    }
   }
   displayFilterRecipes(STATE);
   displayErrorMessage();
@@ -85,24 +85,24 @@ export function onCloseFilterRecipes () {
 export function filterRecipesByMainSearch (option) {
   // == recherche dans les recttes en display = true ===
   if (option.length >= 3) {
-    STATE.forEach(recipe => {
-      const includesInIngredient = recipe.ingredients.map(e => e.ingredient.toLowerCase()).includes(option.toLowerCase());
-      const includesInTitle = recipe.name.toLowerCase().includes(option.toLowerCase());
-      const includesInDescription = recipe.description.toLowerCase().includes(option.toLowerCase());
-      if (recipe.display === true) {
+    for (let i = 0; i < STATE.length; i++) {
+      const includesInIngredient = STATE[i].ingredients.map(e => e.ingredient.toLowerCase()).includes(option.toLowerCase());
+      const includesInTitle = STATE[i].name.toLowerCase().includes(option.toLowerCase());
+      const includesInDescription = STATE[i].description.toLowerCase().includes(option.toLowerCase());
+      if (STATE[i].display === true) {
         if (includesInIngredient === false && includesInTitle === false && includesInDescription === false) {
-          recipe.display = false;
+          STATE[i].display = false;
         }
       }
       // === recherche dans les recettes en display = false dans le cas d'une erreur de frappe ====
-      if (recipe.display === false) {
+      if (STATE[i].display === false) {
         if (selectedElements.length === 0) { // si aucune chip n'a déja été sélectionnée ===
           if (includesInIngredient === true || includesInTitle === true || includesInDescription === true) {
-            recipe.display = true;
+            STATE[i].display = true;
           }
         }
       }
-    });
+    }
     displayFilterRecipes(STATE);
     displayErrorMessage();
     // == remise à 0 de l'affichage des recttes en focntion des chips sélectionnées ===
